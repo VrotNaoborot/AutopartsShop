a7178864b2b3e6446e6aca1d602031eab7657f6b
Panu <nktleznin@gmail.com>
2024-11-18 20:16:24 +0300

    add items in index.html

--- AutopartsShop/AutopartsShop/urls.py
+++ AutopartsShop/AutopartsShop/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from shop.views import index
+from shop.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
+    path('', index),
+    path('test/', test)
 ]
